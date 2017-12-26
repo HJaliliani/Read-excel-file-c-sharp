5012cf8f0499526bb90b5fd9bdc6ebab808df0c7
Hjaliliani <jalilian_ham@yahoo.com>
2017-12-26 20:16:44 +0330

    Read Excel file

--- Asposecell/Form1.Designer.cs
+++ Asposecell/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(208, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Read Excel by aspoe cell";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 220);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Asposecell/Form1.cs
+++ Asposecell/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aspose.Cells;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,43 @@ namespace Asposecell
         {
             InitializeComponent();
         }
+ 
+        //Read All Rows And Columns, row by row and column by column and show them on MessageBox By Aspose.cells component
+        private void button2_Click(object sender, EventArgs e)
+        {
+              //Open your template file.
+            Workbook workbook = new Workbook(@"d:\test.xlsx");
+            //Determine Sheet that we want to work with
+            Worksheet worksheet = workbook.Worksheets[0];
+            //Cell object to access the cells of the sheet
+            Cells cells = worksheet.Cells;
+
+            string tmpvalue="";
+            for (int rw = 0; rw <= cells.MaxRow; rw++)
+            {
+                for (int clmn = 0; clmn < cells.MaxColumn; clmn++)
+                {
+                    //Read the cells and prevent from null exception error
+                    tmpvalue = cells[rw, clmn].Value == null ? string.Empty : cells[rw, clmn].Value.ToString();
+                
+                    //show all  cells data
+                    //  MessageBox.Show("[" +rw +","+clmn+"]="+ tmpvalue);
+                }
+                
+            }
+            MessageBox.Show("Finish reading all of the excell sheet ceels");
+
+            //Get the AA column index. 
+            //int col = CellsHelper.ColumnNameToIndex("A");
+           
+            // Access the "A1" cell in the sheet.
+            //   Cell cell = cells["A2"];
+            // Input the "Hello" text into the "A2" cell
+            //  cell.PutValue("Hello");
+
+            // Save the Excel file.
+            // workbook.Save(@"d:\" + "output.xlsx", SaveFormat.Xlsx);
+        }
     }
+    
 }
